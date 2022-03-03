def9edbbc90526d7091e5ff87990a123a790ab6c
Alex Guaylliri <xsander321@outlook.es>
2022-03-02 22:19:46 -0400

    Contador mediante for, while, do while.

--- WinFormsApp1_tarea_06/Form1.Designer.cs
+++ WinFormsApp1_tarea_06/Form1.Designer.cs
@@ -29,13 +29,72 @@ namespace WinFormsApp1_tarea_06
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.bttn_contar = new System.Windows.Forms.Button();
+            this.txtBx_entrada = new System.Windows.Forms.TextBox();
+            this.txtBx_salida = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bttn_contar
+            // 
+            this.bttn_contar.Location = new System.Drawing.Point(257, 40);
+            this.bttn_contar.Name = "bttn_contar";
+            this.bttn_contar.Size = new System.Drawing.Size(94, 29);
+            this.bttn_contar.TabIndex = 0;
+            this.bttn_contar.Text = "Contar";
+            this.bttn_contar.UseVisualStyleBackColor = true;
+            this.bttn_contar.Click += new System.EventHandler(this.bttn_contar_Click);
+            // 
+            // txtBx_entrada
+            // 
+            this.txtBx_entrada.Location = new System.Drawing.Point(126, 40);
+            this.txtBx_entrada.Name = "txtBx_entrada";
+            this.txtBx_entrada.Size = new System.Drawing.Size(125, 27);
+            this.txtBx_entrada.TabIndex = 1;
+            this.txtBx_entrada.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBx_entrada_KeyPress);
+            // 
+            // txtBx_salida
+            // 
+            this.txtBx_salida.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtBx_salida.Location = new System.Drawing.Point(126, 75);
+            this.txtBx_salida.Multiline = true;
+            this.txtBx_salida.Name = "txtBx_salida";
+            this.txtBx_salida.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBx_salida.Size = new System.Drawing.Size(225, 97);
+            this.txtBx_salida.TabIndex = 2;
+            this.txtBx_salida.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBx_salida_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ingrese numero";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(473, 184);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBx_salida);
+            this.Controls.Add(this.txtBx_entrada);
+            this.Controls.Add(this.bttn_contar);
+            this.Name = "Form1";
+            this.Text = "Contador de numeros";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button bttn_contar;
+        private System.Windows.Forms.TextBox txtBx_entrada;
+        private System.Windows.Forms.TextBox txtBx_salida;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WinFormsApp1_tarea_06/Form1.cs
+++ WinFormsApp1_tarea_06/Form1.cs
@@ -12,10 +12,114 @@ namespace WinFormsApp1_tarea_06
 {
     public partial class Form1 : Form
     {
+        public string s;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void bttn_contar_Click(object sender, EventArgs e)
+        {
+            //Por verdad (Contar) => Se inhabilita el textBox de entrada, se cambia el mensaje del boton
+            //              "Contar" a "clear", cuenta y muestra los valores en el textBox de salida.
+            //Por falso (Clear) => Se recetean los valores.
+            if (bttn_contar.Text.Equals("Contar")) {
+                //Se valida que el TextBox "entrada" tiene un valor.
+                if (!txtBx_entrada.Text.Equals("")) {
+                    txtBx_entrada.Enabled = false;
+                    bttn_contar.Text = "Contando...";
+                    bttn_contar.Enabled = false;
+
+                    int entrada = int.Parse(txtBx_entrada.Text);
+
+                    contar_for(entrada);
+                    txtBx_salida.AppendText("\r\n");
+                    contar_while(entrada);
+                    txtBx_salida.AppendText("\r\n");
+                    contar_do_while(entrada);
+
+                    bttn_contar.Text = "Clear";
+                    bttn_contar.Enabled = true;
+                }
+            }
+            else{
+                txtBx_entrada.Text = "";
+                txtBx_salida.Text = "";
+                txtBx_entrada.Enabled = true;
+                bttn_contar.Text = "Contar";
+                bttn_contar.Enabled = true;
+            }
+        }
+
+        private void contar_for(int entrada) {
+            txtBx_salida.AppendText("Contar - for: \r\n");
+            //string salida = "";
+            for (int i = 0; i <= entrada; i++)
+            {
+                if (i < entrada)
+                {
+                    txtBx_salida.AppendText(i.ToString() + ", ");
+                    //txtBx_salida.Text = txtBx_salida.Text + i.ToString() + ", ";
+                    //salida = salida + i.ToString() + ", ";
+                }
+                else
+                {
+                    txtBx_salida.AppendText(i.ToString() + ".");
+                    //txtBx_salida.Text = txtBx_salida.Text + i.ToString() + ". ";
+                    //salida = salida + i.ToString() + ".";
+                }
+            }
+            //txtBx_salida.Text = "";
+            //txtBx_salida.Text = salida;
+        }
+        private void contar_while(int entrada)
+        {
+            int i = 0;
+            txtBx_salida.AppendText("Contar - while: \r\n"); 
+            while (i <= entrada) {
+                if (i < entrada)
+                {
+                    txtBx_salida.AppendText(i.ToString() + ", ");
+                }
+                else
+                {
+                    txtBx_salida.AppendText(i.ToString() + ".");
+                }
+                i++;
+            }
+        }
+
+        private void contar_do_while(int entrada)
+        {
+            int i = 0;
+            txtBx_salida.AppendText("Contar - do while: \r\n");
+            do {
+                if (i < entrada)
+                {
+                    txtBx_salida.AppendText(i.ToString() + ", ");
+                }
+                else
+                {
+                    txtBx_salida.AppendText(i.ToString() + ".");
+                }
+                i++;
+            } while (i<=entrada);
+        }
+        private void txtBx_salida_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //el textBox de salida no puede recibir valores.
+            if (e.KeyChar >= 0 && e.KeyChar <= 255) {
+                e.Handled = true;
+            }
+        }
+
+        private void txtBx_entrada_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //el textBox de entrada solo puede recibir numeros.
+            if ((e.KeyChar >= 32 && e.KeyChar <= 47) || (e.KeyChar >=58 && e.KeyChar <=255))
+            {
+                e.Handled = true;
+            }
+        }
     }
 }
